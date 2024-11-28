f8c403a2227bd2ef101fc55929a173e86bd0958b
Mitch <mitchjoelklein@hotmail.com>
2024-11-28 13:26:16 +1100

    Functioning API call using serarch input

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface User {
+    readonly id: string;
+    avatar_url: string;
+    html: string;
+}
+
+export interface GithubUserResponse {
+    id: number;
+    login: string;
+    avatar_url: string;
+    html_url: string;
+}
