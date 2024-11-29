9c39852afdbb782447bf9cbddcd5d7d16493a3f0
Mitch <mitchjoelklein@hotmail.com>
2024-11-29 19:46:47 +1100

    Refining UserList.ts

--- src/components/userList.tsx
+++ src/components/userList.tsx
@@ -16,10 +16,13 @@ export const UserList: React.FC<UserListProps> = ({ users }) => {
             <a href={user.html_url} target="_blank">
             <h5 className="card-title">{user.login}</h5></a>
   
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p><div className="d-flex justify-content-center py-4">
+            <p className="card-text">Location:</p>
+            
+            <p className="card-text">Email: </p>
+            <p className="card-text">Company:</p>
+
+
+            <div className="d-flex justify-content-center py-4">
             <button className="mx-3 btn btn-success rounded-circle" style={{ width: "4rem", height: "4rem", backgroundColor: "red", border: "1px solid red"}}>-</button>
             <button onClick={() => storeData(user)} className="mx-3 btn btn-success rounded-circle" style={{ width: "4rem", height: "4rem"}}>+</button>
             </div>
